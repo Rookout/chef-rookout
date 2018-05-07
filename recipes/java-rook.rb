4ef9596bc10e7dda2e0765eb092467f3914f1e4a
Itiel <itielshw@gmail.com>
2018-05-08 01:10:52 +0300

    fix-parntess

--- recipes/java-rook.rb
+++ recipes/java-rook.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 #Download the agent setup file
-remote_file '#{node['rookout']['java_rook_path']}' do
+remote_file "#{node['rookout']['java_rook_path']}" do
     source "http://repository.sonatype.org/service/local/artifact/maven/redirect?r=central-proxy&g=com.rookout&a=rook&v=#{node['rookout']['java_rook_version']}"
     owner   'root'
     group   'root'
