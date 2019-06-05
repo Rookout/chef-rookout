77ea41f8fdcedd35fcfaacc57f947750c2634edc
Ori Shoshan <ori.shoshan@rookout.com>
2019-06-05 10:13:16 +0300

    Add docker agent recipe

---
+++ recipes/docker-agent.rb
@@ -0,0 +1,11 @@
+# Run controller
+docker_image ‘rookout/controller’ do
+ action :pull
+end
+
+docker_container ‘controller’ do
+ repo ‘rookout/controller’
+ port ‘7486:7486’
+ env ‘ROOKOUT_TOKEN=#{node['rookout']['agent_token']}’
+ action :run
+end
