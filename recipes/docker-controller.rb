5df79c6a39bfc291f538ad65d9cc512ffa112d8d
Itiel shwartz <itielshwartz@users.noreply.github.com>
2019-06-10 14:04:01 +0300

    Update docker-controller.rb

--- recipes/docker-controller.rb
+++ recipes/docker-controller.rb
@@ -6,6 +6,6 @@ end
 docker_container ‘controller’ do
  repo ‘rookout/controller’
  port ‘7486:7486’
- env ‘ROOKOUT_TOKEN=#{node['rookout']['agent_token']}’
+ env ‘ROOKOUT_TOKEN=#{node['rookout']['controller_token']}’
  action :run
 end
